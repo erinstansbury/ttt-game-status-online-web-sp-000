e290f5660437214e71547e76acd02f054d9b1b0b
Erin Stansbury <ekstansbury@gmail.com>
2019-12-11 20:36:25 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,8 +47,10 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) || full?(board) || draw?(board)
+  if won?(board) ||  draw?(board)
     return true
-  else 
+  elsif full?(board)
+    return true
+  else
     return false
 end
